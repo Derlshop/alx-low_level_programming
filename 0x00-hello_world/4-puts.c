7bae73974488c8aa1e370464b5ec75f970a1a633
Samuel <delighteousruler@yahoo.com>
2022-09-10 06:27:45 -0700

    C programme

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/* This programme displays a sentence */
+/* Authur Nweke Samuel 10th Sept. 2022 */
+
+#include <stdio.h>
+
+int main ()
+   {
+	   puts("\"programming is like building            a multilingual puzzle");
+	   
+	   
+
+           return (0);
+	   
+
+
+   } 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/* This programme display a sentence */
+/* Nweke Samuel 10th Sept. 2022 */
+
+#include <stdio.h>
+
+      main()
+	
+   { 
+         printf("with proper grammar, but the
+		 outcome is a piece of art, \n");
+	
+
+
+	retutn (0); 
+
+    }	
