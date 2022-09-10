24558d434d6ce8dace9a2c33e6824de301b7e935
Samuel <delighteousruler@yahoo.com>
2022-09-10 07:23:34 -0700

    Resolved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
-/* This programme displays a sentence */
-/* Authur Nweke Samuel 10th Sept. 2022 */
-
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"programming is like building            a multilingual puzzle");
-	   
-	   
-
-return (0);
-	   
-
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 } 
