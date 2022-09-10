03795daa128bddd19623528ef4a7414f99e2e9c4
Samuel <delighteousruler@yahoo.com>
2022-09-10 09:11:11 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
-/* This programme display a sentence */
-/* Nweke Samuel 10th Sept. 2022 */
-
 #include <stdio.h>
 
-      main()
-	
-   { 
-         printf("with proper grammar, but the
-		 outcome is a piece of art, \n");
-	
-
-
-	retutn (0); 
-
-    }	
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
