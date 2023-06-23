9314f48eba5e0b5f3f280ab0d0cbe1b497fd423d
samuel <delighteousruler@yahoo.com>
2023-06-23 03:14:27 -0700

    var

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,18 +2,18 @@
 #include <stdarg.h>
 
 /**
- * This Programme prints the sum of all arguments
- * Authur Nweke Samue
- * Date: 20 April 2023
+ *File- This Programme prints the sum of all arguments
+ * return sum
+
  */
-int sum_them_all(const unsigned int a ...)
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list pos;
 	unsigned int i,	sum = 0;
 	
-	va_start(pos, a);
+	va_start(pos, n);
 
-	for (i = 0; i < a; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(pos, int);
 
 	va_end(pos);
@@ -21,8 +21,8 @@ int sum_them_all(const unsigned int a ...)
 	return (sum);
 }
 
-int main(void)
-{
-	putchar(sum_them_all(2,6,8,9,13,77,100));
-	return 0;
-}
+
+
+
+
+
